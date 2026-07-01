ef1e95bf2b814085ff4d594035778988ae458255
Jay Gopalakrishnan <gjay@pdx.edu>
2026-07-01 23:44:28 +0200

    bragg exact demo & notebook

---
+++ demos/bragg/bragg_exact_scalar_leakymode.py
@@ -0,0 +1,46 @@
+"""Demo: Bragg fiber leaky scalar mode via semi-analytical
+approach of transfer-matrix implemented in  BraggExactScalar.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import newton
+from fibermode.bragg import BraggExactScalar
+
+A = BraggExactScalar(scale=1e-6,
+                     ts=[5e-5, 1e-5, 2e-5],
+                     ns=[1, 1.44, 1],
+                     mats=['air', 'glass', 'air'],
+                     wl=1.2e-6)
+
+# Complex root finding:
+
+nu = 0  # scalar fundamental mode is azimuthally symmetric
+outer = 'h1'  # outgoing Hankel; gives Im(beta) > 0 for leaky modes
+k_low = A.k0 * A.ns[0] * A.scale  # Visual identification in Notebook 2.1
+guess = (.999955 * k_low + .99996 * k_low) / 2
+beta1 = newton(A.determinant, guess, args=(nu, outer), tol=1e-15)
+print(f'Scaled beta = {beta1}')
+print(f'Residual    = {abs(A.determinant(beta1, nu, outer)):.2e}')
+
+# Physical beta and Z² eigenvalue (Im(Z) < 0 for leaky modes)
+
+beta_phys = beta1 / A.scale  # Undo the L-scaling to recover physical beta
+Z2 = A.sqrZfrom(beta_phys)  # The  Z^2 nondimensional eigenvalue
+print(f'beta (physical)  = {beta_phys}')
+print(f'Z^2              = {Z2}')
+print(f'Confinement loss = {20 * beta_phys.imag / np.log(10):.4f} dB/m')
+
+# Assemble and plot the field:
+
+F = A.fields_matplot(beta1, nu, outer)
+A.plot2D_contour(F['Ez'], figsize=(10, 7))
+plt.show()
+A.plot1D(F['Ez_rad'],
+         double_r=True,
+         rlist=[400, 10000, 400],
+         nu=nu,
+         maxscale=True,
+         linewidth=1.5,
+         color='k',
+         figsize=(6, 7))
